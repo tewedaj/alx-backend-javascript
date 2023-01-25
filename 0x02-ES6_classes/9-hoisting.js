bf0d2b1e0c8782f87a828f1902d8b64623e2b6ed
tewedaj <tewedaj13@gmail.com>
2023-01-25 21:50:54 +0300

    * Fixed the code

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,7 +1,4 @@
 /* eslint-disable */
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -17,11 +14,9 @@ export class HolbertonClass {
   }
 }
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -42,6 +37,14 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 
